8e35158e319495495ffa61852c1a12f761453649
frankkai <gaokai20100801@qq.com>
2017-04-22 23:33:54 +0800

    scss转换成css

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,34 @@
 const path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const extractSASS = new ExtractTextPlugin('main.css');
 const config = {
   entry: {
-    app: './src/index.js',
-    vendors: './src/vendors.js'
+    app: './src/index.js'
+    // vendors: './src/vendors.js'
   },
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'bundle.js'
-  }
+  },
+  module: {
+    rules: [{
+        test: /\.scss$/,
+        use: extractSASS.extract({
+        //   {
+        //     loader: "style-loader" // 从JS字符串生成样式节点
+        // }, {
+        //     loader: "css-loader" // 将CSS转化成CommonJS
+        // }, {
+        //     loader: "sass-loader" // 将Sass编译成CSS
+        // }
+          fallback: 'style-loader',
+          use: ["css-loader",'sass-loader']
+        })
+    }]
+  },
+  plugins: [
+    extractSASS
+  ]
   // module: {
   //   rules: [{
   //     test: /.jsx?$/,
