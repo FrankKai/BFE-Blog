af28a01f9eb43b855e548913bcb6138bda320d6f
frankkai <gaokai20100801@qq.com>
2017-08-30 01:08:23 +0800

    博客预览图，开发日志转移

--- src/main.js
+++ src/main.js
@@ -49,14 +49,3 @@ new Vue({
     })
   }
 })
-/*axios.post('/user', {
-    firstName: 'Fred',
-    lastName: 'Flintstone'
-  })
-  .then(function (response) {
-    console.log(response);
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
-*/
